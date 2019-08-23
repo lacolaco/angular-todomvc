02061b377b95c9418069aa83dc685f84a4f7d221
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-08-23 14:29:12 +0900

    use props()

--- projects/todomvc-ngrx/src/app/store/todo/actions.ts
+++ projects/todomvc-ngrx/src/app/store/todo/actions.ts
@@ -1,34 +1,30 @@
-import { createAction, union } from '@ngrx/store';
+import { createAction, union, props } from '@ngrx/store';
 import { Todo } from '../../domain/todo';
 
-export const add = createAction('[Todo] Add', (todo: Todo) => ({
-  todo,
-}));
+export const add = createAction('[Todo] Add', props<{ todo: Todo }>());
 
-export const remove = createAction('[Todo] Remove', (index: number) => ({
-  index,
-}));
+export const remove = createAction('[Todo] Remove', props<{ index: number }>());
 
 export const removeCompleted = createAction('[Todo] Remove Completed');
 
 export const toggleCompletion = createAction(
   '[Todo] Toggle Completion',
-  (index: number) => ({ index }),
+  props<{ index: number }>(),
 );
 
 export const startEditing = createAction(
   '[Todo] Start Editing',
-  (index: number) => ({ index }),
+  props<{ index: number }>(),
 );
 
 export const finishEditing = createAction(
   '[Todo] Finish Editing',
-  (index: number, newTitle: string) => ({ index, newTitle }),
+  props<{ index: number; newTitle: string }>(),
 );
 
 export const cancelEditing = createAction(
   '[Todo] Cancel Editing',
-  (index: number) => ({ index }),
+  props<{ index: number }>(),
 );
 
 const actions = union({
--- projects/todomvc-ngrx/src/app/usecase/todos.usecase.ts
+++ projects/todomvc-ngrx/src/app/usecase/todos.usecase.ts
@@ -9,27 +9,27 @@ export class TodosUsecase {
 
   addTodo(title: string) {
     const todo = createTodo({ title });
-    this.store.dispatch(TodoActions.add(todo));
+    this.store.dispatch(TodoActions.add({ todo }));
   }
 
   toggleCompletion(index: number) {
-    this.store.dispatch(TodoActions.toggleCompletion(index));
+    this.store.dispatch(TodoActions.toggleCompletion({ index }));
   }
 
   startEditing(index: number) {
-    this.store.dispatch(TodoActions.startEditing(index));
+    this.store.dispatch(TodoActions.startEditing({ index }));
   }
 
   removeTodo(index: number) {
-    this.store.dispatch(TodoActions.remove(index));
+    this.store.dispatch(TodoActions.remove({ index }));
   }
 
   finishEditing(index: number, newTitle: string) {
-    this.store.dispatch(TodoActions.finishEditing(index, newTitle));
+    this.store.dispatch(TodoActions.finishEditing({ index, newTitle }));
   }
 
   cancelEditing(index: number) {
-    this.store.dispatch(TodoActions.cancelEditing(index));
+    this.store.dispatch(TodoActions.cancelEditing({ index }));
   }
 
   removeCompleted() {
